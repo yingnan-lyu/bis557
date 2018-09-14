00b00e0f416dc1c5dcef3abe20ebf28c514a1a1a
Yingnan Lu <Dandelion@Hi-2.local>
2018-09-14 11:40:36 -0400

    new linear model.r

--- R/linear-models.r
+++ R/linear-models.r
@@ -11,5 +11,16 @@
 #' summary(fit)
 #' @export
 linear_model <- function(formula, data) {
-  lm(formula, data)
+    mm <- model.matrix(formula,data)
+  X <- mm
+  y_name <- strsplit(as.character(formula)," ~ ")[[2]]
+  y <- as.matrix(data[y_name])
+  beta_hat <- solve(t(X) %*% X) %*% t(X) %*% y
+
+  coeff <- round(as.numeric(beta_hat),15)
+  names(coeff) <- rownames(beta_hat)
+
+  object <- list(coefficients=coeff)
+  class(object) <- "lm"
+  return(object)
 }
