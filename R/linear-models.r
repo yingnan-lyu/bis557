94d318c2ecee407526d200496bb9b4a5ed44a696
Yingnan Lu <Dandelion@kennedys-phone.wireless.yale.internal>
2018-09-20 17:55:18 -0400

    testagain4.r

--- R/linear-models.r
+++ R/linear-models.r
@@ -5,7 +5,7 @@
 #' @param formula a formula
 #' @param data a data.frame
 #' @return An lm object
-#' @importFrom stats lm rmarkdown
+#' @importFrom stats lm
 #' @examples
 #' fit <- linear_model(Sepal.Length ~., iris)
 #' summary(fit)
