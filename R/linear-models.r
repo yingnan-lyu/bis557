a8b5fe9f092f9b70019e0ba23e239f49a7239fca
Yingnan Lu <Dandelion@kennedys-phone.wireless.yale.internal>
2018-09-20 19:21:20 -0400

    testagain5.r

--- R/linear-models.r
+++ R/linear-models.r
@@ -5,7 +5,7 @@
 #' @param formula a formula
 #' @param data a data.frame
 #' @return An lm object
-#' @importFrom stats lm
+#' @importFrom stats
 #' @examples
 #' fit <- linear_model(Sepal.Length ~., iris)
 #' summary(fit)
@@ -16,37 +16,15 @@ linear_model <- function(formula, data) {
   y_name <- strsplit(as.character(formula)," ~ ")[[2]]
   y <- as.matrix(data[y_name])
   
-  # Deal with colinearity in X
-  #Xs <- data[,-which(colnames(data)==y_name)]
-  #Xs_num <- Xs[,sapply(1:ncol(Xs), function(i) is.numeric(Xs[,i]))]
-  #Xs_char <- Xs[,!sapply(1:ncol(Xs), function(i) is.numeric(Xs[,i]))]
-  #tmp <- cor(Xs)
-  #tmp[upper.tri(tmp)] <- 0
-  #diag(tmp) <- 0
-  
-  #Xs <- Xs[apply(tmp, 2, function(x) any(abs(x) > 0.99))]
-  #data_new <- cbind(y, Xs)
-  
   # Define X and calculate svd of X
-  #X <- model.matrix(formula,data_new)
   X <- stats::model.matrix(formula,data)
-  #s <- svd(X)
   QR <- qr(X)
+
+  # Calculate beta hat
   beta_hat <- t(solve.qr(QR,y))
   beta_hat[beta_hat == 0] <- NA
   
-  # Calculate beta hat
-  #beta_hat <- s$v %*% diag(1/s$d) %*% t(s$u) %*% y
-  #beta_hat_data <- as.data.frame(t(beta_hat), row.names = "1")
-  #names(beta_hat_data) <- colnames(X)
-  
   # Store beta hat in a data frame for coefficients
-  #coeff <- as.data.frame(matrix(NA, 1, ncol(data)))
-  #colnames(coeff) <- colnames(model.matrix(formula, data))
-  
-  #coeff <- merge(coeff, beta_hat_data, all.x=TRUE, all.y = TRUE)[1,]
-  #coeff <- as.numeric(coeff)
-  #names(coeff) <- colnames(model.matrix(formula, data))
   coeff <- as.numeric(beta_hat)
   names(coeff) <- colnames(beta_hat)
   
