652500b747a9a466177eb62c71713a638ce41f04
Yingnan Lu <Dandelion@Hi-2.local>
2018-10-05 12:40:36 -0400

    updatedtest.r

--- tests/testthat/test-homework-1.r
+++ tests/testthat/test-homework-1.r
@@ -6,5 +6,6 @@ test_that("The updated version of linear_model works.", {
   data(lm_patho)
   fit_linear_model <- linear_model(y ~., lm_patho)
   fit_lm <- lm(y ~., lm_patho)
-  expect_equivalent(fit_lm$coefficients, fit_linear_model$coefficients)
+  expect_equivalent(fit_lm$coefficients, fit_linear_model$coefficients,
+                    tolerance = 1e-5)
 })
