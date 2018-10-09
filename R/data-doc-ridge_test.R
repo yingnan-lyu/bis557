a2d7c8c99acd30a40ed0e854fa1d6ed29100e4cc
Yingnan Lu <Dandelion@Hi-2.local>
2018-10-09 12:32:49 -0400

    tryoct9.r

---
+++ R/data-doc-ridge_test.R
@@ -0,0 +1,7 @@
+#' The testing data set for the second homework
+#'
+#' @name ridge_test
+#' @docType data
+#' @rdname ridge_test
+#' @description A testing dataset used to test the ridge regression model in homework2.
+"ridge_test"
---
+++ R/data-doc-ridge_train.R
@@ -0,0 +1,7 @@
+#' The training data set for the second homework
+#'
+#' @name ridge_train
+#' @docType data
+#' @rdname ridge_train
+#' @description A training dataset used to train the ridge regression model in homework2.
+"ridge_train"
