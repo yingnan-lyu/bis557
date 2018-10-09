eec825c01524d15c08b5a4ca93507bdda27d08e1
Yingnan Lu <Dandelion@Hi-2.local>
2018-10-09 15:37:03 -0400

    testfinal.r

--- data-raw/make-df.r
+++ data-raw/make-df.r
@@ -1,8 +1,4 @@
 lm_patho <- read.csv("df.csv")
 dir.create("../data")
-save(lm_patho, file = "../data/lm_patho.rda")
+save(lm_patho, file = "../data/lm_patho.rda", compress='xz')
 
-library(devtools)
-ridge_test <- read.csv("~/Documents/Master2-2/BIS 557-Computational Statistics/homework 2/ridge_test.csv")
-ridge_train <- read.csv("~/Documents/Master2-2/BIS 557-Computational Statistics/homework 2/ridge_train.csv")
-devtools::use_data(ridge_test, ridge_train, overwrite = T)
